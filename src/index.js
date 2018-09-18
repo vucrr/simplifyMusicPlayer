7819c039847328de97c45e2a97a2afcc3a389e54
huxingqiu <huxingqiu@7fgame.com>
2018-09-18 15:58:32 +0800

    开发环境完成

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import _ from 'lodash';
-import './index.css';
-import Icon from './logo_white.png';
+import printMe from './print.js';
 function component() {
     var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hell34322o', 'webpack'], ' ');
     element.classList.add('hello');
 
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
     return element;
   }
   document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode:'none',
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   output: {
-    filename: './js/bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
+    
   }
 };
